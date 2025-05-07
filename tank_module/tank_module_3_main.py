337399ad85c0db463fdc9fee0cf659083560a906
thdfw <th.defauw@gmail.com>
2025-05-07 10:09:05 +0200

    Tank module with three layers

---
+++ tank_module/tank_module_3_main.py
@@ -0,0 +1,307 @@
+import machine
+import utime
+import network
+import ujson
+import urequests
+import ubinascii
+import utime
+import gc
+import os
+
+# ---------------------------------
+# Constants
+# ---------------------------------
+
+# Configuration files
+COMMS_CONFIG_FILE = "comms_config.json"
+APP_CONFIG_FILE = "app_config.json"
+
+# Default parameters
+DEFAULT_ACTOR_NAME = "tank"
+DEFAULT_PICO_AB = "a"
+DEFAULT_ASYNC_CAPTURE_DELTA_MICRO_VOLTS = 500
+DEFAULT_CAPTURE_PERIOD_S = 60
+DEFAULT_SAMPLES = 1000
+DEFAULT_NUM_SAMPLE_AVERAGES = 10
+
+# Other constants
+ADC0_PIN_NUMBER = 26
+ADC1_PIN_NUMBER = 27
+ADC2_PIN_NUMBER = 28
+
+# ---------------------------------
+# Main class
+# ---------------------------------
+
+class TankModule:
+
+    def __init__(self):
+        # Unique ID
+        pico_unique_id = ubinascii.hexlify(machine.unique_id()).decode()
+        self.hw_uid = f"pico_{pico_unique_id[-6:]}"
+        # Pins
+        self.adc0 = machine.ADC(ADC0_PIN_NUMBER)
+        self.adc1 = machine.ADC(ADC1_PIN_NUMBER)
+        self.adc2 = machine.ADC(ADC2_PIN_NUMBER)
+        # Load configuration files
+        self.load_comms_config()
+        self.load_app_config()
+        # Measuring and repoting voltages
+        self.prev_mv0 = -1
+        self.prev_mv1 = -1
+        self.prev_mv2 = -1
+        self.mv0 = None
+        self.mv1 = None
+        self.mv2 = None
+        self.node_names = []
+        self.microvolts_posted_time = utime.time()
+        # Synchronous reporting on the minute
+        self.capture_offset_seconds = 0
+        self.sync_report_timer = machine.Timer(-1)
+
+    def set_names(self):
+        if self.actor_node_name is None:
+            raise Exception("Needs actor node name or pico number to run. Reboot!")
+        self.node_names = [
+            f"{self.actor_node_name}-depth1", 
+            f"{self.actor_node_name}-depth2",
+            f"{self.actor_node_name}-depth3"
+        ]
+
+    # ---------------------------------
+    # Communication
+    # ---------------------------------
+                                                                 
+    def load_comms_config(self):
+        '''Load the communication configuration file (WiFi and API base URL)'''
+        try:
+            with open(COMMS_CONFIG_FILE, "r") as f:
+                comms_config = ujson.load(f)
+        except (OSError, ValueError) as e:
+            raise RuntimeError(f"Error loading comms_config file: {e}")
+        self.wifi_name = comms_config.get("WifiName")
+        self.wifi_password = comms_config.get("WifiPassword")
+        self.base_url = comms_config.get("BaseUrl")
+        if self.wifi_name is None:
+            raise KeyError("WifiName not found in comms_config.json")
+        if self.wifi_password is None:
+            raise KeyError("WifiPassword not found in comms_config.json")
+        if self.base_url is None:
+            raise KeyError("BaseUrl not found in comms_config.json")
+        
+    def connect_to_wifi(self):
+        wlan = network.WLAN(network.STA_IF)
+        wlan.active(True)
+        if not wlan.isconnected():
+            print("Connecting to wifi...")
+            wlan.connect(self.wifi_name, self.wifi_password)
+            while not wlan.isconnected():
+                utime.sleep_ms(500)
+        print(f"Connected to wifi {self.wifi_name}")
+
+    # ---------------------------------
+    # Parameters
+    # ---------------------------------
+    
+    def load_app_config(self):
+        '''
+        Set parameters to their value in the app_config file if it is specified
+        Otherwise set them to their default value
+        '''
+        try:
+            with open(APP_CONFIG_FILE, "r") as f:
+                app_config = ujson.load(f)
+        except:
+            app_config = {}
+        self.actor_node_name = app_config.get("ActorNodeName", DEFAULT_ACTOR_NAME)
+        self.pico_a_b = app_config.get("PicoAB", DEFAULT_PICO_AB)
+        self.async_capture_delta_micro_volts = app_config.get("AsyncCaptureDeltaMicroVolts", DEFAULT_ASYNC_CAPTURE_DELTA_MICRO_VOLTS)
+        self.capture_period_s = app_config.get("CapturePeriodS", DEFAULT_CAPTURE_PERIOD_S)
+        self.samples = app_config.get("Samples", DEFAULT_SAMPLES)
+        self.num_sample_averages = app_config.get("NumSampleAverages", DEFAULT_NUM_SAMPLE_AVERAGES)
+
+    def save_app_config(self):
+        config = {
+            "ActorNodeName": self.actor_node_name,
+            "PicoAB": self.pico_a_b,
+            "CapturePeriodS": self.capture_period_s,
+            "Samples": self.samples,
+            "NumSampleAverages":self.num_sample_averages,
+            "AsyncCaptureDeltaMicroVolts": self.async_capture_delta_micro_volts,
+        }
+        with open(APP_CONFIG_FILE, "w") as f:
+            ujson.dump(config, f)
+    
+    def update_app_config(self):
+        url = self.base_url + f"/{self.actor_node_name}/tank-module-params"
+        payload = {
+            "HwUid": self.hw_uid,
+            "ActorNodeName": self.actor_node_name,
+            "PicoAB": self.pico_a_b,
+            "CapturePeriodS": self.capture_period_s,
+            "Samples": self.samples,
+            "NumSampleAverages": self.num_sample_averages,
+            "AsyncCaptureDeltaMicroVolts": self.async_capture_delta_micro_volts,
+            "TypeName": "tank.module.params",
+            "Version": "100"
+        }
+        headers = {"Content-Type": "application/json"}
+        json_payload = ujson.dumps(payload)
+        try:
+            response = urequests.post(url, data=json_payload, headers=headers)
+            if response.status_code == 200:
+                updated_config = response.json()
+                self.actor_node_name = updated_config.get("ActorNodeName", self.actor_node_name)
+                self.pico_a_b = updated_config.get("PicoAB", self.pico_a_b)
+                self.capture_period_s = updated_config.get("CapturePeriodS", self.capture_period_s)
+                self.samples = updated_config.get("Samples", self.samples)
+                self.num_sample_averages = updated_config.get("NumSampleAverages", self.num_sample_averages)
+                self.async_capture_delta_micro_volts = updated_config.get("AsyncCaptureDeltaMicroVolts", self.async_capture_delta_micro_volts)
+                self.capture_offset_seconds = updated_config.get("CaptureOffsetS", 0)
+                self.save_app_config()
+            response.close()
+        except Exception as e:
+            print(f"Error sending tank module params: {e}")
+
+    # ---------------------------------
+    # Code updates
+    # ---------------------------------
+
+    def update_code(self):
+        url = self.base_url + f"/{self.actor_node_name}/code-update"
+        payload = {
+            "HwUid": self.hw_uid,
+            "ActorNodeName": self.actor_node_name,
+            "TypeName": "new.code",
+            "Version": "100"
+        }
+        json_payload = ujson.dumps(payload)
+        headers = {"Content-Type": "application/json"}
+        response = urequests.post(url, data=json_payload, headers=headers)
+        if response.status_code == 200:
+            # If there is a pending code update then the response is a python file, otherwise json
+            try:
+                ujson.loads(response.content.decode('utf-8'))
+            except:
+                python_code = response.content
+                with open('main_update.py', 'wb') as file:
+                    file.write(python_code)
+                machine.reset()
+
+    # ---------------------------------
+    # Measuring microvolts
+    # ---------------------------------
+
+    def adc0_micros(self):
+        sample_averages = []
+        for _ in range(self.num_sample_averages):
+            readings = []
+            for _ in range(self.samples):
+                # Read the raw ADC value (0-65535)
+                readings.append(self.adc0.read_u16())
+            voltages = list(map(lambda x: x * 3.3 / 65535, readings))
+            mean_1000 = int(10**6 * sum(voltages) / self.samples)
+            sample_averages.append(mean_1000)
+        return int(sum(sample_averages)/self.num_sample_averages)
+    
+    def adc1_micros(self):
+        sample_averages = []
+        for _ in range(self.num_sample_averages):
+            readings = []
+            for _ in range(self.samples):
+                # Read the raw ADC value (0-65535)
+                readings.append(self.adc1.read_u16())
+            voltages = list(map(lambda x: x * 3.3 / 65535, readings))
+            mean_1000 = int(10**6 * sum(voltages) / self.samples)
+            sample_averages.append(mean_1000)
+        return int(sum(sample_averages)/self.num_sample_averages)
+    
+    def adc2_micros(self):
+        sample_averages = []
+        for _ in range(self.num_sample_averages):
+            readings = []
+            for _ in range(self.samples):
+                # Read the raw ADC value (0-65535)
+                readings.append(self.adc2.read_u16())
+            voltages = list(map(lambda x: x * 3.3 / 65535, readings))
+            mean_1000 = int(10**6 * sum(voltages) / self.samples)
+            sample_averages.append(mean_1000)
+        return int(sum(sample_averages)/self.num_sample_averages)  
+    
+    # ---------------------------------
+    # Posting microvolts
+    # ---------------------------------
+
+    def post_microvolts(self, idx=3):
+        url = self.base_url + f"/{self.actor_node_name}/microvolts"
+        if idx==0:
+            mv_list = [self.mv0]
+        elif idx==1:
+            mv_list = [self.mv1]
+        elif idx==2:
+            mv_list = [self.mv2]
+        else:
+            mv_list = [self.mv0, self.mv1, self.mv2]
+        payload = {
+            "HwUid": self.hw_uid,
+            "AboutNodeNameList": [self.node_names[idx]] if idx<=2 else self.node_names,
+            "MicroVoltsList": mv_list, 
+            "TypeName": "microvolts", 
+            "Version": "100"
+        }
+        headers = {'Content-Type': 'application/json'}
+        json_payload = ujson.dumps(payload)
+        try:
+            response = urequests.post(url, data=json_payload, headers=headers)
+            response.close()
+        except Exception as e:
+            print(f"Error posting microvolts: {e}")
+        gc.collect()
+        self.microvolts_posted_time = utime.time()
+        
+    def sync_report(self, timer):
+        self.post_microvolts()
+
+    def start_sync_report_timer(self):
+        '''Initialize the timer to call self.keep_alive periodically'''
+        self.sync_report_timer.init(
+            period=self.capture_period_s * 1000, 
+            mode=machine.Timer.PERIODIC,
+            callback=self.sync_report
+        )
+
+    def main_loop(self):
+        self.mv0 = self.adc0_micros()
+        self.mv1 = self.adc1_micros()
+        self.mv2 = self.adc2_micros()
+        while True:
+            self.mv0 = self.adc0_micros()
+            self.mv1 = self.adc1_micros()
+            self.mv2 = self.adc2_micros()
+            if abs(self.mv0 - self.prev_mv0) > self.async_capture_delta_micro_volts:
+                self.post_microvolts(idx=0)
+                self.prev_mv0 = self.mv0
+            if abs(self.mv1 - self.prev_mv1) > self.async_capture_delta_micro_volts:
+                self.post_microvolts(idx=1)
+                self.prev_mv1 = self.mv1
+            if abs(self.mv2 - self.prev_mv2) > self.async_capture_delta_micro_volts:
+                self.post_microvolts(idx=2)
+                self.prev_mv2 = self.mv2
+            utime.sleep_ms(100)
+
+    def start(self):
+        self.connect_to_wifi()
+        self.update_code()
+        self.update_app_config()
+        self.set_names()
+        self.mv0 = self.adc0_micros()
+        self.mv1 = self.adc1_micros()
+        self.mv2 = self.adc2_micros()
+        self.post_microvolts()
+        utime.sleep(self.capture_offset_seconds)
+        self.start_sync_report_timer()
+        self.main_loop()
+
+if __name__ == "__main__":
+    t = TankModule()
+    t.start()
